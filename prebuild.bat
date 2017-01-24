47d5a12db6fcf301c19b7f308727edbe45c345b4
lionsoft <eleybov@gmail.com>
2017-01-24 14:22:14 +0200

    fix bugs

--- prebuild.bat
+++ prebuild.bat
@@ -1,4 +1,6 @@
 @echo off
+
+@if not exist wwwroot md wwwroot
 @cd /d wwwroot >nul 1>nul 2>nul
 @for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q >nul 1>nul 2>nul || del "%%i" /s/q >nul 1>nul 2>nul)
 
