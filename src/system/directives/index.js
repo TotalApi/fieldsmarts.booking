6d8604f0c2f9b73154c35dc96338a49074590370
MGotovkin <MGotovkin@gmail.com>
2017-01-25 18:53:09 +0200

    removed js

--- src/system/directives/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./directive.base"));
-__export(require("./i18n.directive"));
-//# sourceMappingURL=index.js.map
