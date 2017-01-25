fb5ae96571f97be6431a7dc9224a0f0b87a909de
MGotovkin <MGotovkin@gmail.com>
2017-01-25 17:23:29 +0200

    calendar buttons

--- src/app/pages/wizard-done/wizard-done.page.ts
+++ src/app/pages/wizard-done/wizard-done.page.ts
@@ -1,10 +1,12 @@
 import * as ng from '@angular/core';
 import { AppRoute } from 'src/app/app.routes';
+import {DomSanitizer} from '@angular/platform-browser';
 import {AppWizardService} from "../../services/wizard.service";
 import {GeocodeService} from '../../services/geocode.service';
 import { AgmCoreModule, MapsAPILoader } from 'angular2-google-maps/core';
 import {UssInputComponent} from '../../../system/components/semanticui/input/inputs.component';
-
+import {CalendarEvent} from '../../common/AddToCalendar';
+import {AddToCalendar} from '../../common/AddToCalendar';
 declare var google: any;
 
 @ng.Component({
@@ -18,10 +20,11 @@ export class AppWizardDonePage implements ng.OnInit {
 //    url = 'http://aelitsoft.com';
     url = 'http://spray-net.com';
 
-    constructor(public wizard: AppWizardService) { }
+    constructor(public wizard: AppWizardService, private sanitizer:DomSanitizer) { }
 
     ngOnInit(): void {
         this.fbLikeIframeSrc();
+        this.generateEvent();
     }
 
     private fbLikeIframeSrc() {
@@ -41,5 +44,24 @@ export class AppWizardDonePage implements ng.OnInit {
         js = d.createElement(s); js.id = id;
         js.src = `//connect.facebook.net/${this.wizard.translate.currentLang === 'fr' ? "fr_FR" : "en_US"}/sdk.js#xfbml=1&version=v2.8&appId=773528466036157`;
         fjs.parentNode.insertBefore(js, fjs);
+    }
+
+    private googleCal: string;
+    private iCal: string;
+
+    sanitize(url:string){
+        return this.sanitizer.bypassSecurityTrustUrl(url);
+    }
+
+    private generateEvent() {
+        let event = new CalendarEvent();
+        event.title = "Spray Net Consultation";
+        event.address = this.wizard.data.address;
+        event.description = "Spray Net Consultation";
+        event.start = new Date(this.wizard.data.bookTime);
+        event.end = moment(this.wizard.data.bookTime).add(30, 'minutes').toDate();
+
+        this.googleCal = AddToCalendar.google(event);
+        this.iCal = AddToCalendar.ical(event);
     }
 }
