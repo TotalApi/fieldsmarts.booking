cdb4ba0876b24abe063cff84688092d9936576c6
lionsoft <eleybov@gmail.com>
2017-01-31 10:04:49 +0200

    SFD-735: "How did you hear about us?"

--- src/app/services/lookup.service.ts
+++ src/app/services/lookup.service.ts
@@ -11,20 +11,30 @@ import {Sales} from '../models/Sales';
 import {PostBooking} from '../models/Sales';
 import {PostCodeAssignment} from '../models/Sales';
 import {MarketingInfo} from '../models/Sales';
-import {Lookup} from '../models/Sales';
+import {Lookup, HeardAboutUsItem, LookupItem} from '../models/Sales';
+import * as ng2Translate from 'ng2-translate';
 
 @Injectable()
 @ApiService("api/lookup")
 export class LookupService extends UssApiService {
-    constructor(http: Http) { super(http); }
 
-    @ApiMethod({ method: "GET", route: "{p1}", useBody: false })
-    getLookup(lookup: string): Promise<Lookup> {
-        return this.request<Lookup>({ p1: lookup }).toPromise();
+    public get url() {
+        return this.translate.currentLang === 'fr' ? `api/fr/lookup` : `api/lookup`;
+    }
+    public set url(value: string) { }
+
+    constructor(http: Http, private translate: ng2Translate.TranslateService) {
+        super(http);
     }
 
-    getHeardAboutUs(): Promise<Lookup> {
-        return this.getLookup('HeardAboutUs');
+    @ApiMethod({ method: "GET", route: "{p1}", useBody: false })
+    getLookup<T extends LookupItem>(lookup: string): Promise<Lookup<T>> {
+        
+        return this.request<Lookup<T>>({ p1: lookup }).toPromise();
+    }
+
+    getHeardAboutUs(): Promise<Lookup<HeardAboutUsItem>> {
+        return this.getLookup<HeardAboutUsItem>('HeardAboutUs');
     }
 
 }
