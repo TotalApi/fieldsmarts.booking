313627f9336050fa74d9018de68458dff3ecb4e8
lionsoft <eleybov@gmail.com>
2017-01-31 08:57:23 +0200

    angular 4.0.0-beta5

--- src/system/decorators/notify-changes.decorator.ts
+++ src/system/decorators/notify-changes.decorator.ts
@@ -62,7 +62,7 @@ export function extendForPropertyChanges(target: any, propertyKey?: string | str
                 originalSet.call(this, v);
 
                 const changes = { };
-                changes[<string>propertyKey] = new SimpleChange(oldValue, v);
+                changes[<string>propertyKey] = new SimpleChange(oldValue, v, false);
                 if (this.__nc_values['$notifyChanges.emmiter'] instanceof EventEmitter) {
                     setTimeout(() => this.__nc_values['$notifyChanges.emmiter'].emit(changes));
                 }
